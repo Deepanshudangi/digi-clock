a13d179a14cc19227fc25ed343bff3ff081ebd0c
deepanshudangi <deepanshudangi10@gmail.com>
2021-06-05 14:21:45 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+// import showTime from "./node_modules/digital-clock/index.js";
+function showTime() {
+    let time = new Date();
+    let hour = time.getHours();
+    let min = time.getMinutes();
+    let am_pm = "AM";
+
+    if (hour > 12) {
+        hour -= 12;
+        am_pm = "PM";
+    }
+    if (hour == 0) {
+        hr = 12;
+        am_pm = "AM";
+    }
+
+    hour = hour < 10 ? "0" + hour : hour;
+    min = min < 10 ? "0" + min : min;
+
+    let currentTime = hour + ":"
+        + min + " " + am_pm;
+
+    document.querySelector(".digital-clock")
+        .innerHTML = currentTime;
+}
+
+export default showTime
+setInterval(showTime, 1000);
