61970dda62f86da1c98be6ad8a018df81cadec34
deepanshudangi <deepanshudangi10@gmail.com>
2021-06-05 14:44:38 +0530

    add animations

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ function showTime() {
     let time = new Date();
     let hour = time.getHours();
     let min = time.getMinutes();
+    let sec = time.getSeconds();
     let am_pm = "AM";
 
     if (hour > 12) {
@@ -16,9 +17,10 @@ function showTime() {
 
     hour = hour < 10 ? "0" + hour : hour;
     min = min < 10 ? "0" + min : min;
+    sec = sec < 10 ? "0" + sec : sec;
 
     let currentTime = hour + ":"
-        + min + " " + am_pm;
+        + min + " : " + sec + " " + am_pm;
 
     document.querySelector(".digital-clock")
         .innerHTML = currentTime;
